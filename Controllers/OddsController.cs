95b35304594acbaf53af9e6b7e4ecc3944765e5b
stampnoon <thnong_stamp@hotmail.com>
2020-02-27 13:54:46 +0700

    Add new function select League

--- Controllers/OddsController.cs
+++ Controllers/OddsController.cs
@@ -14,20 +14,41 @@ namespace FootballAPI.Controllers
 
     public class OddsController : ControllerBase
     {
+        private string KeyAPI = "155cd660-57a7-11ea-9697-a77f73d8dc3e";
 
         [HttpGet]
         public ActionResult<IEnumerable<BetData>> GetOdds_SoccerAllFormAPI()
         {
+            List<BetData> BetData = new List<BetData>();
+            string URL = "https://app.oddsapi.io/api/v1/odds?sport=soccer&apikey=" + KeyAPI;
+
+            string JsonStr = SentRequestAPI(URL);
+            BetData = ConvertJsontoObject(JsonStr);
+
+            return BetData;
+        }
+
+        [HttpGet("{league}")]
+        public ActionResult<IEnumerable<BetData>> GetOdds_SoccerLeagueFormAPI(string league)
+        {
+            List<BetData> BetData = new List<BetData>();
+            string URL = "https://app.oddsapi.io/api/v1/odds?sport=soccer&league=" + league + "&apikey=" + KeyAPI;
+
+            string JsonStr = SentRequestAPI(URL);
+            BetData = ConvertJsontoObject(JsonStr);
+
+            return BetData;
+        }
+
+        [HttpGet]
+        private List<BetData> ConvertJsontoObject(string Str)
+        {
+            List<BetData> BetData = new List<BetData>();
             string LeagueName;
             Event Eventdata;
             Odds Oddsdata;
             OddPers OddsPerdata;
-            List<BetData> BetData = new List<BetData>();
-
-            string URL = "https://app.oddsapi.io/api/v1/odds?sport=soccer&apikey=155cd660-57a7-11ea-9697-a77f73d8dc3e";
-            // string URL = "https://app.oddsapi.io/api/v1/odds?sport=soccer&league=soccer-europe-champions-league&apikey=155cd660-57a7-11ea-9697-a77f73d8dc3e";
-            string JsonStr = SentRequestAPI(URL);
-
+            string JsonStr = Str;
             //แปลง Json ให้พร้อมใช้งาน
             JsonStr = JsonStr.Replace("1x2", "onextwo").Replace("\"", "\\");
             JsonStr = JsonStr.Replace(@"\1\", @"\one\").Replace(@"\2\", @"\two\").Replace("\\", "\"");
@@ -131,85 +152,5 @@ namespace FootballAPI.Controllers
             string retString = myStreamReader.ReadToEnd();
             return retString;
         }
-
-
-
-        // [HttpGet("{str}")]
-        // public ActionResult<IEnumerable<BetData>> TestGetSoccerAllFormText(string str)
-        // {
-        //     string JsonStr = str;
-        //     // string URL = "https://app.oddsapi.io/api/v1/odds?sport=soccer&apikey=155cd660-57a7-11ea-9697-a77f73d8dc3e";
-        //     // JsonStr = SentRequestAPI(URL);
-
-        //     string LeagueName;
-        //     Event Eventdata;
-        //     Odds Oddsdata;
-        //     List<BetData> BetData = new List<BetData>();
-
-        //     //แปลง Json ให้พร้อมใช้งาน
-        //     JsonStr = JsonStr.Replace("1x2", "onextwo").Replace("\"", "\\");
-        //     JsonStr = JsonStr.Replace(@"\1\", @"\one\").Replace(@"\2\", @"\two\").Replace("\\", "\"");
-
-        //     //Convert Json to object
-        //     var ObjectOdds = JsonConvert.DeserializeObject<List<RootObject>>(JsonStr);
-
-        //     foreach (var eachobject in ObjectOdds)
-        //     {
-        //         LeagueName = "";
-        //         Eventdata = null;
-        //         Oddsdata = null;
-
-        //         if (eachobject.Sites.onextwo != null) //เลือกเฉพาะตัวที่มีราคาต่อรองแล้ว
-        //         {
-        //             //IF have League
-        //             if (eachobject.League.name != null)
-        //             {
-        //                 LeagueName = eachobject.League.name;
-        //             }
-
-        //             //IF have Event
-        //             if (eachobject.Event != null)
-        //             {
-        //                 Eventdata = eachobject.Event;
-        //             }
-
-        //             //IF have Odds
-        //             if (eachobject.Sites.onextwo != null)
-        //             {
-        //                 if (eachobject.Sites.onextwo.bet365 != null)
-        //                 {
-        //                     Oddsdata = eachobject.Sites.onextwo.bet365.odds;
-        //                 }
-        //                 else if (eachobject.Sites.onextwo.sbobet != null)
-        //                 {
-        //                     Oddsdata = eachobject.Sites.onextwo.sbobet.odds;
-        //                 }
-        //                 else if (eachobject.Sites.onextwo.eeesport != null)
-        //                 {
-        //                     Oddsdata = eachobject.Sites.onextwo.eeesport.odds;
-        //                 }
-        //                 else if (eachobject.Sites.onextwo.betfair != null)
-        //                 {
-        //                     Oddsdata = eachobject.Sites.onextwo.betfair.odds;
-        //                 }
-        //                 else if (eachobject.Sites.onextwo.interwetten != null)
-        //                 {
-        //                     Oddsdata = eachobject.Sites.onextwo.interwetten.odds;
-        //                 }
-        //             }
-
-        //             var item = new BetData
-        //             {
-        //                 League = LeagueName,
-        //                 Event = Eventdata,
-        //                 odds = Oddsdata
-        //             };
-        //             BetData.Add(item);
-        //         }
-        //     }
-
-        //     //convert to object
-        //     return BetData;
-        // }
     }
 }
